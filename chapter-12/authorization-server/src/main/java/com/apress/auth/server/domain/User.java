0bd2275a598ecb26b465e11eceb2fc2621a56415
pedram <pedram.khoshdani@gmail.com>
2024-05-18 21:57:44 +0200

    added homepage

--- chapter-12/authorization-server/src/main/java/com/apress/auth/server/domain/User.java
+++ chapter-12/authorization-server/src/main/java/com/apress/auth/server/domain/User.java
@@ -1,11 +1,11 @@
 package com.apress.auth.server.domain;
 
+import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.persistence.*;
 import java.util.Collection;
 import java.util.Collections;
 
